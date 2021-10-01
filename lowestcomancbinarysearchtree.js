6dd4fd00b40df2c4607dbf3c4550743b3d1e1337
Daniel Pickens <72703981+DanielPickens@users.noreply.github.com>
2021-09-30 21:19:49 -0600

    Create lowestcomancbinarysearchtree.js

---
+++ lowestcomancbinarysearchtree.js
@@ -0,0 +1,23 @@
+var lowestCommonAncestor = function(root, p, q) {
+  const _max = Math.max(p.val, q.val);
+        const _min = Math.min(p.val, q.val);
+        while (root != null) { //setting constraints 
+            if (_min > root.val) {  //invert the nodes in a binary tree, and traverse tree node, where in this case, it will recursively check if the left and right sub-trees are also binary trees themselves. 
+
+                root = root.right; 
+            } else if (_max < root.val) {  ///invert the nodes in a binary tree, and traverse tree node, where in this case, it will  recursively check if the left and right sub-trees are also binary trees themselves
+                root = root.left;
+            } else {
+                return root;     
+            } 
+        }
+        return null;   // root!= null, return null, hence if a binary tree node is NULL then it is a full binary tree. 
+    }
+
+
+/*
+Success
+Details 
+Runtime: 92 ms, faster than 84.27% of JavaScript online submissions for Lowest Common Ancestor of a Binary Search Tree.
+Memory Usage: 49.2 MB, less than 14.84% of JavaScript online submissions for Lowest Common Ancestor of a Binary Search Tree.
+*/
